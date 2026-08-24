0489e4eb194c0adb5653d2379a7c686e015852b2
Banini <banini4lyf@gmail.com>
2026-08-24 13:27:55 +0100

    Updated task sorting

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,7 +19,7 @@ function TodoItem({ todoList, deleteTodo, editTask, todoCategory }) {
   }*/
   return (
     <div>
-      <div className=" flex gap-2">
+      <div className=" flex gap-2 my-4 ">
         <button
           className="p-2 w-25 bg-gray-500 rounded-full"
           onClick={() => {
@@ -40,7 +40,7 @@ function TodoItem({ todoList, deleteTodo, editTask, todoCategory }) {
           </button>
         ))}
       </div>
-      {todoList.length === 0 && (
+      {todoCategory.length === 0 && (
         <>
           <h1 className="text-2xl mb-3 ">
             <span className="text-6xl mb-3">😢</span>
@@ -51,7 +51,7 @@ function TodoItem({ todoList, deleteTodo, editTask, todoCategory }) {
         </>
       )}
 
-      {todoList.length > 0 &&
+      {todoCategory.length > 0 &&
         sortedTodo.map((tsk, index) => (
           <div className="border p-4 flex gap-1 m-4" key={index}>
             <h2 className="text-2xl font-bold">{tsk.title}</h2>
